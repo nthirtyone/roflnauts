4d2c7c76dfd99dd488edcb162fc6e6bd1f08b7ac
Aki <nthirtyone@gmail.com>
2016-05-17 14:13:35 +0200

    Moved quads to new file

--- effect.lua
+++ effect.lua
@@ -11,44 +11,8 @@ Effect = {
 	frame = 1,
 	animation = nil,
 	sprite = love.graphics.newImage("assets/effects.png"),
-	quads = {
-		jump = {
-			[1] = love.graphics.newQuad(  0,  0, 24,24, 168,120),
-			[2] = love.graphics.newQuad( 24,  0, 24,24, 168,120),
-			[3] = love.graphics.newQuad( 48,  0, 24,24, 168,120),
-			[4] = love.graphics.newQuad( 72,  0, 24,24, 168,120),
-			frames = 4
-		},
-		doublejump = {
-			[1] = love.graphics.newQuad(  0, 24, 24,24, 168,120),
-			[2] = love.graphics.newQuad( 24, 24, 24,24, 168,120),
-			[3] = love.graphics.newQuad( 48, 24, 24,24, 168,120),
-			[4] = love.graphics.newQuad( 72, 24, 24,24, 168,120),
-			frames = 4
-		},
-		land = {
-			[1] = love.graphics.newQuad(  0, 48, 24,24, 168,120),
-			[2] = love.graphics.newQuad( 24, 48, 24,24, 168,120),
-			[3] = love.graphics.newQuad( 48, 48, 24,24, 168,120),
-			[4] = love.graphics.newQuad( 72, 48, 24,24, 168,120),
-			[5] = love.graphics.newQuad( 96, 48, 24,24, 168,120),
-			frames = 5
-		},
-		respawn = {},
-		clash = {},
-		trail = {},
-		hit = {}
-	}
+	quads = require "effects"
 }
-
--- NAME      :POSITION :SIZE :FRAMES
--- jump      :x  0 y  0: 24px: 4
--- doublejump:x  0 y 24: 24px: 4
--- land      :x  0 y 48: 24px: 5
--- respawn   :x  0 y 72: 24px: 7
--- clash     :x  0 y 96: 24px: 6
--- trail     :x104 y  0: 16px: 4
--- hit       :x106 y 18: 16px: 3
 
 -- Construct of `Effect`
 function Effect:new(name, x, y)
