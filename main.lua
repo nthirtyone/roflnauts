42c1711ac6fde9c25d36981d4f8bcc451083fe25
Aki <nthirtyone@gmail.com>
2016-05-21 01:44:40 +0200

    Third, fourth test player

--- main.lua
+++ main.lua
@@ -10,6 +10,8 @@ require "effect"
 
 -- Temporary debug
 debug = false
+third = false
+fourth = false
 
 -- Load
 function love.load ()
@@ -23,16 +25,38 @@ function love.load ()
 	w:createPlatform(290/2+140, 180/2+50, {-26,1, 26,1, 26,30, -26,30}, "assets/platform_small.png")
 	w:createPlatform(290/2-140, 180/2+50, {-26,1, 26,1, 26,30, -26,30}, "assets/platform_small.png")
 	w:createPlatform(290/2, 180/2-50, {-17,1, 17,1, 17,16, -17,16}, "assets/platform_top.png")
-	w:createNaut(290/2-10, 180/2 - 80, "leon")
-	w:createNaut(290/2+10, 180/2 - 80, "lonestar")
+	w:createNaut(290/2-15, 180/2 - 80, "leon")
+	w:createNaut(290/2+15, 180/2 - 80, "lonestar")
+	w:createNaut(290/2+05, 180/2 - 80, "clunk")
+	w:createNaut(290/2-05, 180/2 - 80, "yuri")
 	
 	-- Temporary settings for second player
 	w.Nauts[2].key_left = "a"
 	w.Nauts[2].key_right = "d"
 	w.Nauts[2].key_up = "w"
 	w.Nauts[2].key_down = "s"
-	w.Nauts[2].key_jump = "h"
-	w.Nauts[2].key_hit = "g"
+	w.Nauts[2].key_jump = "g"
+	w.Nauts[2].key_hit = "f"
+	
+	-- Temporary settings for third player
+	if third then
+	w.Nauts[3].key_left = "kp4"
+	w.Nauts[3].key_right = "kp6"
+	w.Nauts[3].key_up = "kp8"
+	w.Nauts[3].key_down = "kp5"
+	w.Nauts[3].key_jump = "kp2"
+	w.Nauts[3].key_hit = "kp3"
+	end
+	
+	-- Temporary settings for fourth player
+	if fourth then
+	w.Nauts[4].key_left = "b"
+	w.Nauts[4].key_right = "m"
+	w.Nauts[4].key_up = "h"
+	w.Nauts[4].key_down = "n"
+	w.Nauts[4].key_jump = "k"
+	w.Nauts[4].key_hit = "l"
+	end
 end
 
 -- Update
