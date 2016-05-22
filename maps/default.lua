227f6693728b777da1b4ec015087e187ace1fe8f
Aki <nthirtyone@gmail.com>
2016-05-22 22:20:56 +0200

    Emo request

--- maps/default.lua
+++ maps/default.lua
@@ -1,7 +1,7 @@
 -- Default map from original roflnauts
 return {
 	center_x = 0,
-	center_y = 30,
+	center_y = 60,
 	width  = 320,
 	height = 240,
 	color_top = {193, 100,  99, 255},
@@ -15,27 +15,27 @@ return {
 	},
 	platforms = {
 		{
-			x = 0,
+			x = -91,
 			y = 0,
-			shape = {-91,1, 90,1, 90,10, 5,76, -5,76, -91,10},
+			shape = {0,1, 181,1, 181,10, 96,76, 86,76, 0,10},
 			sprite = "assets/platform_big.png"
 		},
 		{
-			x = 140,
+			x = 114,
 			y = 50,
-			shape = {-26,1, 26,1, 26,30, -26,30},
+			shape = {0,1, 52,1, 52,30, 0,30},
 			sprite = "assets/platform_small.png"
 		},
 		{
-			x = -140,
+			x = -166,
 			y = 50,
-			shape = {-26,1, 26,1, 26,30, -26,30},
+			shape = {0,1, 52,1, 52,30, 0,30},
 			sprite = "assets/platform_small.png"
 		},
 		{
-			x = 0,
+			x = -17,
 			y = -50,
-			shape = {-17,1, 17,1, 17,16, -17,16},
+			shape = {0,1, 34,1, 34,16, 0,16},
 			sprite = "assets/platform_top.png"
 		}
 	}
