5f8c2f891d029ccce51a92eef2f11ab2115342f7
Aki <nthirtyone@gmail.com>
2016-05-24 00:16:52 +0200

    Fighting with camera

--- conf.lua
+++ conf.lua
@@ -2,7 +2,7 @@ function love.conf(t)
 	t.title = "notnauts"
 	t.version = "0.10.1"
 	t.window.icon = "assets/icon.png"
-	t.window.width = 290*4
-	t.window.height = 180*4
+	t.window.width = 290*2
+	t.window.height = 180*2
 	t.console = true
 end
