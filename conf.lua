75caedeaf6050620e0c33df986794559c2f49236
Aki <nthirtyone@gmail.com>
2016-06-26 17:35:03 +0200

    Borderless fullscreen

--- conf.lua
+++ conf.lua
@@ -1,9 +1,8 @@
 -- Game configuration
 function love.conf(t)
-	t.title = "notnauts"
+	t.title = "Roflnauts 2"
 	t.version = "0.10.1"
-	t.window.icon = "assets/icon.png"
-	t.window.width = 320*4
-	t.window.height = 180*4
+	t.window.fullscreentype = "desktop"
+	t.window.fullscreen = true
 	t.console = false
 end
