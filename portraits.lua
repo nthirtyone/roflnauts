602478c97e06ba8a7edccaaf31c95444e2164f72
Aki <nthirtyone@gmail.com>
2016-07-07 03:48:33 +0200

    More names

--- portraits.lua
+++ portraits.lua
@@ -9,113 +9,113 @@ return {
 		active = love.graphics.newQuad(298,166,32,32,w,h)
 	},
 	-- 1. ROW
-	frog = {
+	froggo = {
 		normal = love.graphics.newQuad(  1,  1,32,32,w,h),
 		active = love.graphics.newQuad(  1, 34,32,32,w,h)
 	},
-	lonestar = {
+	cowboy = {
 		normal = love.graphics.newQuad( 34,  1,32,32,w,h),
 		active = love.graphics.newQuad( 34, 34,32,32,w,h)
 	},
-	leon = {
+	honic = {
 		normal = love.graphics.newQuad( 67,  1,32,32,w,h),
 		active = love.graphics.newQuad( 67, 34,32,32,w,h)
 	},
-	scoop = {
+	gelato = {
 		normal = love.graphics.newQuad(100,  1,32,32,w,h),
 		active = love.graphics.newQuad(100, 34,32,32,w,h)
 	},
-	gnaw = {
+	veno = {
 		normal = love.graphics.newQuad(133,  1,32,32,w,h),
 		active = love.graphics.newQuad(133, 34,32,32,w,h)
 	},
-	raelynn = {
+	lady = {
 		normal = love.graphics.newQuad(166,  1,32,32,w,h),
 		active = love.graphics.newQuad(166, 34,32,32,w,h)
 	},
-	ayla = {
+	girl = {
 		normal = love.graphics.newQuad(199,  1,32,32,w,h),
 		active = love.graphics.newQuad(199, 34,32,32,w,h)
 	},
-	clunk = {
+	megoman = {
 		normal = love.graphics.newQuad(232,  1,32,32,w,h),
 		active = love.graphics.newQuad(232, 34,32,32,w,h)
 	},
-	voltar = {
+	brainos = {
 		normal = love.graphics.newQuad(265,  1,32,32,w,h),
 		active = love.graphics.newQuad(265, 34,32,32,w,h)
 	},
 	-- 2. ROW
-	coco = {
+	woman = {
 		normal = love.graphics.newQuad(  1, 67,32,32,w,h),
 		active = love.graphics.newQuad(  1,100,32,32,w,h)
 	},
-	skolldir = {
+	bison = {
 		normal = love.graphics.newQuad( 34, 67,32,32,w,h),
 		active = love.graphics.newQuad( 34,100,32,32,w,h)
 	},
-	yuri = {
+	bobito = {
 		normal = love.graphics.newQuad( 67, 67,32,32,w,h),
 		active = love.graphics.newQuad( 67,100,32,32,w,h)
 	},
-	derpl = {
+	slugzor = {
 		normal = love.graphics.newQuad(100, 67,32,32,w,h),
 		active = love.graphics.newQuad(100,100,32,32,w,h)
 	},
-	vinnie = {
+	capone = {
 		normal = love.graphics.newQuad(133, 67,32,32,w,h),
 		active = love.graphics.newQuad(133,100,32,32,w,h)
 	},
-	genji = {
+	bug = {
 		normal = love.graphics.newQuad(166, 67,32,32,w,h),
 		active = love.graphics.newQuad(166,100,32,32,w,h)
 	},
-	swiggins = {
+	calamari = {
 		normal = love.graphics.newQuad(199, 67,32,32,w,h),
 		active = love.graphics.newQuad(199,100,32,32,w,h)
 	},
-	rocco = {
+	quack = {
 		normal = love.graphics.newQuad(232, 67,32,32,w,h),
 		active = love.graphics.newQuad(232,100,32,32,w,h)
 	},
-	ksenia = {
+	scissors = {
 		normal = love.graphics.newQuad(265, 67,32,32,w,h),
 		active = love.graphics.newQuad(265,100,32,32,w,h)
 	},
 	-- 3. ROW
-	ted = {
+	marine = {
 		normal = love.graphics.newQuad(  1,133,32,32,w,h),
 		active = love.graphics.newQuad(  1,166,32,32,w,h)
 	},
-	penny = {
+	scooter = {
 		normal = love.graphics.newQuad( 34,133,32,32,w,h),
 		active = love.graphics.newQuad( 34,166,32,32,w,h)
 	},
-	sentry = {
+	phonebooth = {
 		normal = love.graphics.newQuad( 67,133,32,32,w,h),
 		active = love.graphics.newQuad( 67,166,32,32,w,h)
 	},
-	skree = {
+	weed = {
 		normal = love.graphics.newQuad(100,133,32,32,w,h),
 		active = love.graphics.newQuad(100,166,32,32,w,h)
 	},
-	nibbs = {
+	gummybear = {
 		normal = love.graphics.newQuad(133,133,32,32,w,h),
 		active = love.graphics.newQuad(133,166,32,32,w,h)
 	},
-	yoolip = {
+	gramps = {
 		normal = love.graphics.newQuad(166,133,32,32,w,h),
 		active = love.graphics.newQuad(166,166,32,32,w,h)
 	},
-	chucho = {
+	biker = {
 		normal = love.graphics.newQuad(199,133,32,32,w,h),
 		active = love.graphics.newQuad(199,166,32,32,w,h)
 	},
-	lux = {
+	vrooom = {
 		normal = love.graphics.newQuad(232,133,32,32,w,h),
 		active = love.graphics.newQuad(232,166,32,32,w,h)
 	},
-	ix = {
+	link = {
 		normal = love.graphics.newQuad(265,133,32,32,w,h),
 		active = love.graphics.newQuad(265,166,32,32,w,h)
 	},
