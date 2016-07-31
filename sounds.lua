e1bcab1e723ebaa165f1338fc08972e07495a0df
Aki <nthirtyone@gmail.com>
2016-07-31 22:06:39 +0200

    Wav to Ogg

--- sounds.lua
+++ sounds.lua
@@ -1,6 +1,6 @@
 return {
-	love.sound.newSoundData("sounds/death.wav"),
-	love.sound.newSoundData("sounds/hit.wav"),
-	love.sound.newSoundData("sounds/land.wav"),
-	love.sound.newSoundData("sounds/attack.wav")
+	love.sound.newSoundData("sounds/death.ogg"),
+	love.sound.newSoundData("sounds/hit.ogg"),
+	love.sound.newSoundData("sounds/land.ogg"),
+	love.sound.newSoundData("sounds/attack.ogg")
 }
