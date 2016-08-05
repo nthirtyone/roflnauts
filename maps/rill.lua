1fb2fbc01b7ee90ccb9fa7e9d8e6f926efc946e8
Aki <nthirtyone@gmail.com>
2016-08-05 16:53:11 +0200

    Rill map placeholders

--- maps/rill.lua
+++ maps/rill.lua
@@ -2,49 +2,49 @@ return {
 	-- CENTER AND SIZE
 	name = "rill",
 	center_x = 0,
-	center_y = 60,
+	center_y = 80,
 	width  = 320,
 	height = 240,
 	-- RESPAWN POINTS
 	respawns = {
-		{x = -100, y = 10},
-		{x = -100, y = 10},
-		{x =  100, y = 10},
-		{x =  100, y = 10}
+		{x = -135, y = 10},
+		{x = -135, y = 10},
+		{x =  135, y = 10},
+		{x =  135, y = 10}
 	},
 	-- GRAPHICS
 	clouds = false,
 	background = "assets/backgrounds/rill.png",
 	platforms = {
 		{
-			x = -120,
-			y = 30,
-			shape = {0,0, 40,0, 40,15, 0,15},
-			sprite = "assets/platform_top.png"
+			x = -160,
+			y = 25,
+			shape = {2,2, 47,2, 47,18, 2,18},
+			sprite = "assets/platforms/rill-block-left.png"
 		},
 		{
-			x = 80,
-			y = 30,
-			shape = {0,0, 40,0, 40,15, 0,15},
-			sprite = "assets/platform_top.png"
+			x = 110,
+			y = 25,
+			shape = {0,2, 46,2, 42,18, 0,18},
+			sprite = "assets/platforms/rill-block-right.png"
 		},
 		{
-			x = -15,
+			x = -32,
 			y = 70,
-			shape = {0,0, 30,0, 30,15, 0,15},
-			sprite = "assets/platform_top.png"
+			shape = {2,2, 62,2, 42,18, 22,18},
+			sprite = "assets/platforms/rill-center.png"
 		},
 		{
 			x = -110,
 			y = 90,
-			shape = {0,0, 10,-10, 90,80, 80,90},
-			sprite = "assets/platform_top.png"
+			shape = {2,2, 18,2, 76,41, 75,61, 2,9},
+			sprite = "assets/platforms/rill-slide-left.png"
 		},
 		{
-			x = 110,
+			x = 34,
 			y = 90,
-			shape = {0,0, -10,-10, -90,80, -80,90},
-			sprite = "assets/platform_top.png"
+			shape = {2,44, 64,2, 75,2, 7,57},
+			sprite = "assets/platforms/rill-slide-right.png"
 		}
 	},
 	decorations = {}
