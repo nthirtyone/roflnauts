22c36dfaac394ef8148d50a95c862e937b4e908a
Aki <nthirtyone@gmail.com>
2016-08-14 23:08:38 +0200

    Moved love callbacks for readability

--- menu.lua
+++ menu.lua
@@ -107,59 +107,6 @@ function Menu:getBounce(f)
 	return math.sin(self.header_move*f*math.pi)
 end
 
--- Update
-function Menu:update(dt)
-	local state = true
-	if self:getSelectorsNumberActive() > 1 then
-		for _,selector in pairs(self:getSelectorsActive()) do
-			state = state and selector.state
-		end
-	else
-		state = false
-	end
-	if state then
-		self.countdown = self.countdown - dt
-	else
-		self.countdown = Menu.countdown -- Menu.countdown is initial
-	end
-	if state and self.countdown < 0 then
-		self:startGame()
-	end
-	-- Bounce header
-	self.header_move = self.header_move + dt
-	if self.header_move > 2 then
-		self.header_move = self.header_move - 2
-	end
-end
-
--- Draw
-function Menu:draw()
-	-- locals
-	local w, h = love.graphics.getWidth()/self.scale, love.graphics.getHeight()/self.scale
-	local scale = self.scale
-	-- map selection
-	love.graphics.setFont(Font)
-	love.graphics.printf("Map: " .. self.maplist[self.map], (w/2)*scale, (h/2-22)*scale, 150, "center", 0, scale, scale, 75, 4)
-	-- character selection
-	for _,selector in pairs(self:getSelectorsAll()) do
-		selector:draw()
-	end
-	-- header
-	love.graphics.setFont(Bold)
-	local angle = self:getBounce(2)
-	local dy = self:getBounce()*4
-	love.graphics.printf("ROFLNAUTS2",(w/2)*scale,(32+dy)*scale,336,"center",(angle*5)*math.pi/180,scale,scale,168,12)
-	-- footer
-	love.graphics.setFont(Font)
-	love.graphics.printf("Use W,S,A,D,G,H or Arrows,Enter,Rshift or Gamepad\n\nA game by the Awesomenauts community\nSeltzy, ParaDoX, MilkingChicken, Burningdillo, Bronkey, Aki, 04font\nBased on a game by Jan Willem Nijman, Paul Veer and Bits_Beats XOXO", (w/2)*scale, (h-42)*scale, 336, "center", 0, scale, scale, 168, 4)
-	-- countdown
-	local countdown = math.floor(self.countdown)
-	if self.countdown < Menu.countdown then -- Menu.countdown is initial
-		love.graphics.setFont(Bold)
-		love.graphics.print(countdown,(w/2-6.5)*self.scale,(h/2+30)*self.scale,0,self.scale,self.scale)
-	end
-end
-
 -- Speed up countdown
 function Menu:countdownJump()
 	if self.countdown ~= Menu.countdown then -- Menu.countdown is initial
@@ -195,6 +142,59 @@ end
 function Menu:startGame()
 	local world = World:new(self.maplist[self.map], self:getNauts())
 	changeScene(world)
+end
+
+-- LÖVE2D callbacks
+-- Update
+function Menu:update(dt)
+	local state = true
+	if self:getSelectorsNumberActive() > 1 then
+		for _,selector in pairs(self:getSelectorsActive()) do
+			state = state and selector.state
+		end
+	else
+		state = false
+	end
+	if state then
+		self.countdown = self.countdown - dt
+	else
+		self.countdown = Menu.countdown -- Menu.countdown is initial
+	end
+	if state and self.countdown < 0 then
+		self:startGame()
+	end
+	-- Bounce header
+	self.header_move = self.header_move + dt
+	if self.header_move > 2 then
+		self.header_move = self.header_move - 2
+	end
+end
+-- Draw
+function Menu:draw()
+	-- locals
+	local w, h = love.graphics.getWidth()/self.scale, love.graphics.getHeight()/self.scale
+	local scale = self.scale
+	-- map selection
+	love.graphics.setFont(Font)
+	love.graphics.printf("Map: " .. self.maplist[self.map], (w/2)*scale, (h/2-22)*scale, 150, "center", 0, scale, scale, 75, 4)
+	-- character selection
+	for _,selector in pairs(self:getSelectorsAll()) do
+		selector:draw()
+	end
+	-- header
+	love.graphics.setFont(Bold)
+	local angle = self:getBounce(2)
+	local dy = self:getBounce()*4
+	love.graphics.printf("ROFLNAUTS2",(w/2)*scale,(32+dy)*scale,336,"center",(angle*5)*math.pi/180,scale,scale,168,12)
+	-- footer
+	love.graphics.setFont(Font)
+	love.graphics.printf("Use W,S,A,D,G,H or Arrows,Enter,Rshift or Gamepad\n\nA game by the Awesomenauts community\nSeltzy, ParaDoX, MilkingChicken, Burningdillo, Bronkey, Aki, 04font\nBased on a game by Jan Willem Nijman, Paul Veer and Bits_Beats XOXO", (w/2)*scale, (h-42)*scale, 336, "center", 0, scale, scale, 168, 4)
+	-- countdown
+	local countdown = math.floor(self.countdown)
+	if self.countdown < Menu.countdown then -- Menu.countdown is initial
+		love.graphics.setFont(Bold)
+		love.graphics.print(countdown,(w/2-6.5)*self.scale,(h/2+30)*self.scale,0,self.scale,self.scale)
+	end
 end
 
 -- Controller callbacks
