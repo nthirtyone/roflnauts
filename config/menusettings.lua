bdc2e0ee9420eaf99ad9502ad061f124e17e00b8
Aki <nthirtyone@gmail.com>
2016-10-06 14:05:56 +0200

    New empty menu for settings

---
+++ config/menusettings.lua
@@ -0,0 +1,7 @@
+local menu = ...
+
+local button = require "button"
+local selector = require "selector"
+
+local width, height = love.graphics.getWidth()/getRealScale(), love.graphics.getHeight()/getRealScale()
+local bx = width/2-29
