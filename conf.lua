1a7dc6c7b9e36872a5ad45122aa5f34f532ed7ab
Aki <nthirtyone@gmail.com>
2016-10-10 12:02:18 +0200

    Identity added

--- conf.lua
+++ conf.lua
@@ -5,6 +5,7 @@ function love.conf(t)
 	-- t.window.width = 320*3
 	-- t.window.height = 180*3
 	-- t.window.borderless = true
+	t.identity = "not-nautz"
 	t.window.fullscreentype = "desktop"
 	t.window.fullscreen = true
 	t.console = false
