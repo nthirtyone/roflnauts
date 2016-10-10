941d0214a4a1b499190f4f3c6133b5e512d897fe
Aki <nthirtyone@gmail.com>
2016-10-10 22:42:58 +0200

    Add joystick controllers only if js present

--- settings.lua
+++ settings.lua
@@ -12,10 +12,14 @@ function Settings.load()
 		local getSettings = love.filesystem.load("settings")
 		for _,set in pairs(getSettings()) do
 			local isJoystick = set[7]
+			local joystick
 			if isJoystick then
+				joystick = love.joystick.getJoysticks()[1]
 				-- Add first free joystick from joysticks list
 			end
-			Controller.registerSet(set[1], set[2], set[3], set[4], set[5], set[6], joystick)
+			if not isJoystick or joystick then
+				Controller.registerSet(set[1], set[2], set[3], set[4], set[5], set[6], joystick)
+			end
 		end
 	end
 end
