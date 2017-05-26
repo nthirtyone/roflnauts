8e7c7d2a4ca74fcb58f36424b05a96d358208e39
Aki <nthirtyone@gmail.com>
2017-05-26 22:46:50 +0200

    Comment on Entity

--- not/Entity.lua
+++ not/Entity.lua
@@ -3,6 +3,7 @@ require "not.Sprite"
 --- `Entity`
 -- Basic, visible object to be used within World instance. Can be anything. Represented as `Sprite`.
 -- We still need to keep old, global way of using modules but there are `returns` on the end at least.
+-- It probably would be nice to move `World` dependency out of it but for now it should stay this way. Later `World` instance could be just passed to methods that need it.
 Entity = Sprite:extends()
 
 Entity.world =--[[not.World]]nil
