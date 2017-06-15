2c458f0c756ebe577083a7cc972a07e870cb0ba9
Aki <nthirtyone@gmail.com>
2017-06-15 17:21:02 +0200

    Music renamed to MusicPlayer changed behaviour to use static table for sources need to change dependencies now

---
+++ not/MusicPlayer.lua
@@ -0,0 +1,34 @@
+require "not.object"
+
+--- `MusicPlayer`
+-- Simple music player object that plays and loops selected track.
+MusicPlayer = Object:extends()
+
+MusicPlayer.TRACKS = {}
+
+-- TODO: trackName should be passed without file extension.
+function MusicPlayer:new (trackName)
+	self:setTrack(trackName)
+end
+
+function MusicPlayer:delete ()
+	self.source:stop()
+end
+
+function MusicPlayer:setTrack (trackName)
+	if self.source then
+		self.source:stop()
+	end
+	if MusicPlayer.TRACKS[trackName] then
+		self.source = MusicPlayer.TRACKS[trackName]
+	else
+		local source = love.audio.newSource("assets/music" .. trackName)
+		source:setLooping(true)
+		source:setVolume(.7)
+		self.source = source
+		MusicPlayer.TRACKS[trackName] = source
+	end
+	self.source:play()
+end
+
+return MusicPlayer
