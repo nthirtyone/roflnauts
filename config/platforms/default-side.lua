1ffc08dd2343b76b2f94d9c290ef346346585270
Aki <nthirtyone@gmail.com>
2017-09-05 19:05:21 +0200

    Converted default map to new format for cloud testing

---
+++ config/platforms/default-side.lua
@@ -0,0 +1,5 @@
+return
+{
+	sprite = "assets/platforms/default-side.png",
+	shape = {0,1, 51,1, 51,18, 0,18}
+}
---
+++ config/platforms/default-top.lua
@@ -0,0 +1,5 @@
+return
+{
+	sprite = "assets/platforms/default-top.png",
+	shape = {0,1, 33,1, 33,14, 0,14}
+}
