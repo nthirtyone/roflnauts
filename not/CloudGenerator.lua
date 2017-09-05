69eebe12baa0804cacf7285d1fdc20f42334dc27
Aki <nthirtyone@gmail.com>
2017-09-05 05:01:49 +0200

    CloudGenerator created

---
+++ not/CloudGenerator.lua
@@ -0,0 +1,12 @@
+--- Generates clouds over time with randomized positions and styles.
+CloudGenerator = require "not.Object":extends()
+
+function CloudGenerator:new (world, styles)
+	self.world = world
+end
+
+function CloudGenerator:update (dt)
+	
+end
+
+return CloudGenerator
