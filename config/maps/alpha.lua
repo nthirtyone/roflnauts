438c230b21f93a98f78b03bfc3cf1cad6a6e2655
Aki <nthirtyone@gmail.com>
2017-09-06 03:45:35 +0200

    Translated Alpha Abyss map to dev format

---
+++ config/maps/alpha.lua
@@ -0,0 +1,44 @@
+return
+{
+	name = "Alpha Abyss",
+	theme = "alpha.ogg",
+	portrait = 7, -- TODO: See `maps/ribbit`.
+	center = {x = 0, y = -80},
+	width  = 360,
+	height = 240,
+	respawns = {
+		{x = -30, y = 0},
+		{x =  30, y = 0},
+		{x =   0, y = 0},
+		{x = -120, y = -50},
+		{x = 120, y = -50},
+		{x =  0, y = -75}
+	},
+	clouds = false,
+	create = {
+		{
+			ratio = 0,
+			background = "assets/backgrounds/alpha.png",
+		},
+		{
+			x = -60,
+			y = 0,
+			platform = "alpha-big",
+		},
+		{
+			x = -145,
+			y = -50,
+			platform = "alpha-small",
+		},
+		{
+			x = 85,
+			y = -50,
+			platform = "alpha-small",
+		},
+		{
+			x = -30,
+			y = -80,
+			platform = "alpha-small",
+		}
+	},
+}
---
+++ config/platforms/alpha-small.lua
@@ -0,0 +1,31 @@
+return
+{
+	sprite = "assets/platforms/alpha-small.png",
+	shape = {0,0, 59,0, 59,19, 0,19},
+	animations = {
+		default = {
+			[1] = love.graphics.newQuad(0, 0, 60, 20, 600, 20),
+			[2] = love.graphics.newQuad(0, 0, 60, 20, 600, 20),
+			[3] = love.graphics.newQuad(60, 0, 60, 20, 600, 20),
+			[4] = love.graphics.newQuad(60, 0, 60, 20, 600, 20),
+			[5] = love.graphics.newQuad(120, 0, 60, 20, 600, 20),
+			[6] = love.graphics.newQuad(120, 0, 60, 20, 600, 20),
+			[7] = love.graphics.newQuad(180, 0, 60, 20, 600, 20),
+			[8] = love.graphics.newQuad(180, 0, 60, 20, 600, 20),
+			[9] = love.graphics.newQuad(240, 0, 60, 20, 600, 20),
+			[10] = love.graphics.newQuad(240, 0, 60, 20, 600, 20),
+			[11] = love.graphics.newQuad(300, 0, 60, 20, 600, 20),
+			[12] = love.graphics.newQuad(300, 0, 60, 20, 600, 20),
+			[13] = love.graphics.newQuad(360, 0, 60, 20, 600, 20),
+			[14] = love.graphics.newQuad(360, 0, 60, 20, 600, 20),
+			[15] = love.graphics.newQuad(420, 0, 60, 20, 600, 20),
+			[16] = love.graphics.newQuad(420, 0, 60, 20, 600, 20),
+			[17] = love.graphics.newQuad(480, 0, 60, 20, 600, 20),
+			[18] = love.graphics.newQuad(480, 0, 60, 20, 600, 20),
+			[19] = love.graphics.newQuad(540, 0, 60, 20, 600, 20),
+			[20] = love.graphics.newQuad(540, 0, 60, 20, 600, 20),
+			frames = 20,
+			repeated = true
+		}
+	}
+}
