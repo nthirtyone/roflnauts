ddf8c5f880e7175ce316e61471743654b2486d14
Aki <nthirtyone@gmail.com>
2017-09-13 00:08:06 +0200

    Backgroundless, themeless and iconless AI Station 205 added

---
+++ config/platforms/205-left.lua
@@ -0,0 +1,8 @@
+return
+{
+	sprite = "assets/platforms/205-left.png",
+	shape = {
+		{8,0, 54,0, 54,31, 8,27},
+		{55,29, 94,29, 92,36, 55,36}
+	}
+}
---
+++ config/platforms/205-top.lua
@@ -0,0 +1,5 @@
+return
+{
+	sprite = "assets/platforms/205-top.png",
+	shape = {0,1, 72,1, 70,8, 2,8}
+}
